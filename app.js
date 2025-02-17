2fecafaefb47e5e18b9e5028c25ef2842109671a
Matheus <matheusalmeidad20@gmail.com>
2025-02-17 17:04:07 -0300

    Corrected hit message

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
-console.log(numeroSecreto)
-let chute
+console.log(numeroSecreto);
+let chute;
+let tentativas = 0;
 
 while(chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 30');
+    tentativas++;
 
     if (chute == numeroSecreto) {
-        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
-    } 
-    else {
+        break;
+    } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
         } else {
-            alert(`O número secreto é maior que ${chute}`)
+            alert(`O número secreto é maior que ${chute}`);
         }
     }
 }
 
+if (tentativas > 1) {
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+} else {
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+}
