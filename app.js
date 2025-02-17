88b094f00ecb7fce8b38779596abee3adcbfe2f3
Matheus <matheusalmeidad20@gmail.com>
2025-02-17 17:16:31 -0300

    Change from if-else condition to ternary operation

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
 let chute;
-let tentativas = 0;
+let tentativas = 1;
 
 while(chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 30');
-    tentativas++;
 
     if (chute == numeroSecreto) {
         break;
@@ -17,10 +16,14 @@ while(chute != numeroSecreto) {
             alert(`O número secreto é maior que ${chute}`);
         }
     }
+    tentativas++;
 }
 
-if (tentativas > 1) {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-} else {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
-}
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+// if (tentativas > 1) {
+//     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+// } else {
+//     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+// }
